f1c9adafbc8b54c66678ed43c987239a722b2535
Stabilo <zteo015@e.ntu.edu.sg>
2021-11-08 23:24:41 +0800

    Added github code

--- gifget/src/Components/Fav.jsx
+++ gifget/src/Components/Fav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Fav(props) {
-  return <a href={props.title}>{props.title}</a>;
+  return <a className="fav" href={props.title}>{props.title}</a>;
 }
 
 export default Fav;
--- gifget/src/Components/gif.jsx
+++ gifget/src/Components/gif.jsx
@@ -4,7 +4,7 @@ function Gif(props) {
   return (
     <div className="gif-row">
       <img src={props.url} alt="..." onClick={props.clickFav}/>
-      <footer>Powered By GIPHY. Made with ReactJS.</footer>
+      <footer>Powered By GIPHY. Made with ReactJS. Github code <a className="git" href="https://github.com/zyteo/GifGet" target="_blank">here.</a></footer>
     </div>
   );
 }
